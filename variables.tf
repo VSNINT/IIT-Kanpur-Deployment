c729c98bebbbbfbe0ea434f0e584542a67a6a30d
Ankitpatel1122 <110924535+Ankitpatel1122@users.noreply.github.com>
2025-02-17 16:20:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,3 +29,24 @@ variable "vm_size" {
 variable "admin_username" {
   default = "azureuser"
 }
+
+# Azure Authentication Variables
+variable "client_id" {
+  description = "The client ID of the Azure service principal"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The client secret of the Azure service principal"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant ID of the Azure subscription"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The subscription ID of the Azure subscription"
+  type        = string
+}
