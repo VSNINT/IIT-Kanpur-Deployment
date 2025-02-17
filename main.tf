0def28edb9002516f9c5dd0808634792fc331ab3
Ankitpatel1122 <110924535+Ankitpatel1122@users.noreply.github.com>
2025-02-17 16:20:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+/*provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.region
